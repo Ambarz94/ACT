c1ea9d18eb9c02b2a82906db722c6cbebca22436
Ambarz94 <48347530+Ambarz94@users.noreply.github.com>
2020-08-15 10:51:43 +0700

    Add files via upload

---
+++ message.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+  $("#open").click(function(){
+    // $("#top-header").hide();
+  });
+  $("#open").click(function(){
+    $("#bg-menu").show();
+  });
+  $("#close").click(function(){
+    $("#bg-menu").hide();
+  });
+  $("#close").click(function(){
+    $("#top-header").show();
+  });
+});
